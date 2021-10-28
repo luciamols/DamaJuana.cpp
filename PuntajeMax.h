7fe05643858f1ab2926e1394160d0151fe37e156
luciamols <81710678+luciamols@users.noreply.github.com>
2021-10-28 15:59:59 -0300

    Create PuntajeMax.h

---
+++ PuntajeMax.h
@@ -0,0 +1,16 @@
+#ifndef PUNTAJEMAX_H
+#define PUNTAJEMAX_H
+
+
+class PuntajeMax
+{
+    private:
+        int puntajeMax;
+    public:
+        void setPuntajeMax(int);
+        int getPuntajeMax();
+        void grabarPuntaje();
+        void leerPuntaje();
+};
+
+#endif // PUNTAJEMAX_H
